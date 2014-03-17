13e62ed640a7f68a7a02dc87954d7f3dc8b87b35
Nathan Peck <nathan.peck1@g.austincc.edu>
2014-03-17 17:03:56 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
 standard-deviation-stream
 =========================
 
-A Node.js class for pulling stats from a stream.
+A Node.js method for finding the standard deviation of numbers arriving in a stream. Based on Donald Knuth's
+solution in Art of Computer Programming as [described by John D. Cook](http://www.johndcook.com/standard_deviation.html).
+
+Basic Example
+-------------
+
+Just get the standard deviation on some numbers.
+
+```
+var DeviationStream = require('standard-deviation-stream);
+
+var numbers = new DeviationStream();
+
+numbers.push(10);
+numbers.push(15);
+numbers.push(20);
+numbers.push(25);
+
+console.log('Count: '+numbers.count());
+console.log('Mean: '+numbers.mean());
+console.log('Variance: '+numbers.variance());
+console.log('Standard Deviation: '+numbers.standardDeviation());
+
+numbers.clear();
+```
+
+Redis Enhanced Example
+----------------------
+
+If you are fetching a stream of numbers in batches it can be useful to save the state of the standard deviation stream
+between batches.
+
+For example lets say you have a web endpoint that returns results from a database collection in pages of
+100 at a time, and needs to calculate standard deviation on the results. With the redis enhanced style you can fetch 100 items,
+calculate standard deviation on that page of 100 results, and cache the stream data.
+
+Now the next time the web endpoint is called to fetch the next page of 100 results you can restore from
+cache and continue calculating the standard deviation on the next 100 items, taking into account the mean and variance
+of the first 100 items that you fetched in the previous request.
+
+```
+var async = require('async');
+var DeviationStream = require('standard-deviation-stream);
+
+var numbers = new DeviationStream('someResults', redisConnection);
+
+async.series(
+	[
+		function (done) {
+			numbers.restore(done);
+		},
+
+		function (done) {
+			numbers.push(10);
+			numbers.push(15);
+			numbers.push(20);
+			numbers.push(25);
+			numbers.save(done);
+		}
+	],
+	function () {
+		console.log('Done');
+	}
+);
+
+```
+
+By default the save function stores the standard deviation stream data in a Redis key with an expiration of one hour.
+This allows you to continue adding more items to a stream based on that key at any time within an hour.
---
+++ tests/test.js
@@ -0,0 +1,146 @@
+var expect          = require('chai').expect,
+    redis           = require('redis'),
+    async           = require('async'),
+    _               = require('lodash'),
+    DeviationStream = require('../lib/standard-deviation-stream.js');
+
+var redisConfig = {
+  host: "127.0.0.1",
+  port: 6379
+};
+
+function generateRandomGuid() {
+  var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+  return guid;
+}
+
+describe('Standard Deviation Stream', function () {
+  var redisClient = null;
+
+  //Make sure we can connect to Redis.
+  before(function (callback) {
+    redisClient = redis.createClient(redisConfig.port, redisConfig.host);
+
+    redisClient.on('error', function (err) {
+      callback('Failed to connect to Redis:' + err);
+    });
+
+    redisClient.on('ready', function () {
+      console.log('Connected to Redis successfully.');
+      callback(null);
+    });
+  });
+
+  var testDeviation;
+  var restoredDeviation;
+
+  describe('Creating stream and restoring state from a blank key', function () {
+    before(function (done) {
+      var key = 'testData:' + generateRandomGuid();
+      testDeviation = new DeviationStream(key, redisClient);
+      restoredDeviation = new DeviationStream(key, redisClient);
+
+      testDeviation.restore(function () {
+        done();
+      });
+    });
+
+    it('should restore it to a blank state', function () {
+      expect(testDeviation.count()).to.equal(0);
+      expect(testDeviation.mean()).to.equal(0);
+      expect(testDeviation.variance()).to.equal(0);
+      expect(testDeviation.standardDeviation()).to.equal(0);
+    });
+  });
+
+  describe('Pushing some data into the deviation stream', function () {
+    before(function () {
+      _.each(
+        [0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100],
+        function (item) {
+          testDeviation.push(item);
+        }
+      );
+    });
+
+    it('should produce some reasonable values', function () {
+      expect(testDeviation.count()).to.equal(30);
+      expect(testDeviation.mean()).to.equal(50);
+      expect(testDeviation.standardDeviation()).to.be.above(49).below(51);
+    });
+  });
+
+  describe('Saving state in one stream and then restoring it in another stream using the same key', function () {
+    before(function (done) {
+      async.series(
+        [
+          function save(callback) {
+            testDeviation.save(callback);
+          },
+
+          function restore(callback) {
+            restoredDeviation.restore(callback);
+          }
+        ],
+        function (err) {
+          expect(err).to.equal(null);
+          done();
+        }
+      );
+    });
+
+    it('should retain the same values', function () {
+      expect(restoredDeviation.count()).to.equal(30);
+      expect(restoredDeviation.mean()).to.equal(50);
+      expect(restoredDeviation.standardDeviation()).to.be.above(49).below(51);
+    });
+  });
+
+  describe('Restoring state after clearing state', function () {
+    before(function (done) {
+      async.series(
+        [
+          function save(callback) {
+            testDeviation.clear(callback);
+          },
+
+          function restore(callback) {
+            testDeviation.restore(callback);
+          }
+        ],
+        function (err) {
+          expect(err).to.equal(null);
+          done();
+        }
+      );
+    });
+
+    it('should have the stream in a reset state', function () {
+      expect(testDeviation.count()).to.equal(0);
+      expect(testDeviation.mean()).to.equal(0);
+      expect(testDeviation.variance()).to.equal(0);
+      expect(testDeviation.standardDeviation()).to.equal(0);
+    });
+  });
+
+  describe('Another sanity test', function () {
+    before(function () {
+      _.each(
+        [0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100,
+         0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100],
+        function (item) {
+          testDeviation.push(item);
+        }
+      );
+    });
+
+    it('should produce some reasonable values', function () {
+      expect(testDeviation.count()).to.equal(50);
+      expect(testDeviation.mean()).to.equal(50);
+      expect(testDeviation.standardDeviation()).to.be.above(35).below(36);
+    });
+  });
+});
